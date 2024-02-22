13dd35fc6ea886aa0c3c9382fbdba84c883e9ab6
Bhuneshar <imankitkalirawana@gmail.com>
2024-02-22 15:20:57 +0530

    Refactor Footer and Breadcrumbs components

--- components/landingpage/Footer.tsx
+++ components/landingpage/Footer.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
 const footerNavs = [
   {
     label: "Resources",
@@ -121,7 +119,29 @@ const Footer = () => {
                   className="input input-bordered pl-10"
                 />
               </div>
-              <button className="btn btn-primary">Subscribe</button>
+              <button className="btn btn-primary hidden sm:block">
+                Subscribe
+              </button>
+              <button
+                className="btn btn-primary sm:hidden tooltip"
+                data-tip="Subscribe"
+              >
+                {/* mail icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"
+                  />
+                </svg>
+              </button>
             </form>
           </div>
         </div>
@@ -232,5 +252,4 @@ const Footer = () => {
   );
 };
 
-// export default dynamic(() => Promise.resolve(Footer), { ssr: false });
 export default Footer;
