b5fa1a8ed7180bfbca3fc494d95bd641598fb7b2
Bhuneshar <imankitkalirawana@gmail.com>
2024-02-23 20:32:01 +0530

    Revert "Add AWS SDK and implement file download functionality"
    This reverts commit f00175da23f987cb58825181a957f8259cfd8acd.

--- components/resources/ResourceType.tsx
+++ components/resources/ResourceType.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 const ResourceType = ({
   ResourceType,
