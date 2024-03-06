2cdb66d44749c6e4f6f6d0c10b98afb95e1fc63b
Bhuneshar <imankitkalirawana@gmail.com>
2024-03-06 22:52:48 +0530

    Refactor resourceSchema in Resources.js

--- models/Resources.js
+++ models/Resources.js
@@ -1,48 +1,43 @@
 import mongoose from "mongoose";
 
-const resourceSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    link: String,
-    description: String,
-    file: String,
-    type: {
-      type: String,
-      required: true,
-    },
-    isArchived: {
-      type: Boolean,
-      default: false,
-    },
-    filesize: {
-      type: String,
-      default: "0",
-    },
-    addedDate: {
-      type: Date,
-      default: Date.now(),
-    },
-
-    updatedAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    by: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Users",
-    },
-    subject: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Subjects",
-    },
+const resourceSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  link: String,
+  description: String,
+  file: String,
+  type: {
+    type: String,
+    required: true,
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  filesize: {
+    type: String,
+    default: "0",
+  },
+  addedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subjects",
+  },
+});
 
 const Resource =
   mongoose.models.Resource || mongoose.model("Resource", resourceSchema);
--- models/Subjects.js
+++ models/Subjects.js
@@ -1,40 +1,35 @@
 // subjectModel.js
 
-const subjectSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    code: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    url: String,
-    description: String,
-    isArchived: {
-      type: Boolean,
-      default: false,
-    },
-    file: String,
-    addedDate: {
-      type: Date,
-      default: Date.now(),
-    },
-    by: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Users",
-    },
-    updatedDate: {
-      type: Date,
-      default: Date.now(),
-    },
+const subjectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  url: String,
+  description: String,
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  file: String,
+  addedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  updatedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const Subject =
   mongoose.models.Subject || mongoose.model("Subject", subjectSchema);
