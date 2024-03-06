87d226f61082c35571a37f0a9a583a60b34c854d
Bhuneshar <imankitkalirawana@gmail.com>
2024-03-06 23:09:18 +0530

    Add additional allowed origins for CORS

--- cors.js
+++ cors.js
@@ -5,6 +5,8 @@ const cors = Cors({
   allowedMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   origin: [
     "https://devocode.vercel.app",
+    "https://devocode.in",
+    "https://www.devocode.in",
     "http://localhost:5173",
     "http://localhost:3000",
     // Add any other allowed origins
