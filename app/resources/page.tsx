e25cd4357e80e9ff7385e03a9568542352d37920
Bhuneshar <imankitkalirawana@gmail.com>
2024-03-07 08:32:24 +0530

    Refactor API call in page.tsx and remove unused code

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,7 +3,6 @@ import API_BASE_URL from "@/utils/config";
 import Homepage from "@/components/resources/Homepage";
 
 async function getSubjects() {
-  // const res = await axios.get(`${API_BASE_URL}/resources/subjects`);
   const res = await fetch(`${API_BASE_URL}/resources/subjects`, {
     cache: "no-cache",
     method: "GET",
@@ -12,20 +11,6 @@ async function getSubjects() {
 }
 
 export default async function Page() {
-  // const [subjects, setSubjects] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchSubjects = async () => {
-  //     try {
-  //       const res = await axios.get(`${API_BASE_URL}/resources/subjects`);
-  //       setSubjects(res.data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchSubjects();
-  // }, []);
-
   const subjects = await getSubjects();
 
   return (
--- app/resources/subjects/page.tsx
+++ app/resources/subjects/page.tsx
@@ -1,33 +1,17 @@
-"use client";
 import API_BASE_URL from "@/utils/config";
 import Subjects from "@/components/resources/Subjects";
 import NotFound from "@/components/assets/NotFound";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
-// async function getSubjects() {
-//   const res = await fetch(`${API_BASE_URL}/resources/subjects`, {
-//     cache: "no-cache",
-//     method: "GET",
-//   });
-//   return res.json();
-// }
+async function getSubjects() {
+  const res = await fetch(`${API_BASE_URL}/resources/subjects`, {
+    cache: "no-cache",
+    method: "GET",
+  });
+  return res.json();
+}
 
-export default function Page() {
-  // const subjects = await getSubjects();
-  const [subjects, setSubjects] = useState([]);
-
-  useEffect(() => {
-    const fetchSubjects = async () => {
-      try {
-        const res = await axios.get(`${API_BASE_URL}/resources/subjects`);
-        setSubjects(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchSubjects();
-  }, []);
+export default async function Page() {
+  const subjects = await getSubjects();
 
   // filter subjects with isArchived = false
   const filteredSubjects = subjects?.filter(
