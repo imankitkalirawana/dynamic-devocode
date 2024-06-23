22fc0f2a67b0d1543c86b46cb2840d0bcd366008
Bhuneshar <imankitkalirawana@gmail.com>
2024-06-23 16:38:44 +0530

    final fix to contact message error

--- pages/api/mail/contact.js
+++ pages/api/mail/contact.js
@@ -4,7 +4,7 @@ const handler = async (req, res) => {
   if (req.method === "POST") {
     try {
       // get ip address of the user
-      // const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+      const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
       const { firstname, lastname, phoneNumber, email, message } = req.body;
       if (!firstname || !email || !message) {
         res.status(400).json({ message: "All fields are required" });
@@ -19,6 +19,7 @@ const handler = async (req, res) => {
             <p><strong>Phone:</strong> ${phoneNumber}</p>
             <p><strong>Email:</strong> ${email}</p>
             <p><strong>Message:</strong> ${message}</p>
+            <p><strong>IP Address:</strong> ${ip}</p>
           </body>
         </html>
       `;
